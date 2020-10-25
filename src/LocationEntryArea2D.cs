236704caaceee6230d027fb7f0a1d60fc04dba16
Penn Wilbert <pwilbert@gmail.com>
2020-10-25 10:45:44 -0400

    object name refactoring

--- src/LocationEntryArea2D.cs
+++ src/LocationEntryArea2D.cs
@@ -20,15 +20,21 @@ public class LocationEntryArea2D : Area2D
 //      
 //  }
 
+    //listener for player moving around the board. If player not moving and on Area2d box
+    //then move player inside of the building. 
+    //TODO disable buttons until player clicks done? This would prevent player from "moving"
+    //through the building out to the road. Done button would move player back to the road.
     public void OnArea2DBodyEntered(KinematicBody2D body) {
         var _player = GetNode<Character>(GameData.characterNodePath);
         if (!_player.moving) {
             var _infoScene = GetNode<InfoScene>("/root/RootScene/InfoScene");
             var _travelPath = GetNode<TileMap>(GameData.travelPathTileMap);
             var _tileMapPos = _travelPath.WorldToMap(GetNode<Character>(GameData.characterNodePath).Position);
+            // var _buttonGroup = GetNode<ButtonGroup>("/root/RootScene/TravelPath/ButtonNode");
             Vector2 _currentPlayerPos = _player.Position;
             GD.Print("_currentPlayerPos: " + _currentPlayerPos);
             _player.Position = new Vector2(_currentPlayerPos.x, _currentPlayerPos.y - 40);
+
             GD.Print(_tileMapPos);
             GD.Print(Name);
         }
